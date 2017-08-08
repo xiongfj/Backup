9a20e7d74af7588d6998f79939b1902164c6bd7b
xiongfj <837943056@qq.com>
2017-08-08 15:01:16 +0800

    s

--- 0---------------AngularJS/事件---------------.js
+++ 0---------------AngularJS/事件---------------.js
@@ -1,6 +1,37 @@
 
 
-ng-click
+ng-click		// 响应标签的点击事件
+{
+/*
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<script src="http://apps.bdimg.com/libs/angular.js/1.4.6/angular.min.js"></script>
+</head>
+<body>
+
+<div ng-app="myApp" ng-controller="myCtrl" ng-init="num=0"><!-- 定义 num 变量 -->
+
+<button ng-click="count = count + 1; num=num+1">点我！</button>
+
+<p>{{ count }}</p>
+	<h1>{{num}}</h1>
+
+</div>
+<script>
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope) {
+    $scope.count = 0;		// 定义变量
+});
+</script>
+
+</body>
+</html>
+
+*/
+}
+
 ng-dbl-click
 ng-mousedown
 ng-mouseenter
