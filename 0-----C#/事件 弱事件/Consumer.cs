f0fb4b977b9ec387fcf8564ae3008ac3c67b2533
xiongfj <837943056@qq.com>
2017-08-10 19:03:05 +0800

    asd

---
+++ 0-----C#/事件 弱事件/Consumer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+
+namespace Wrox.ProCSharp.Delegates
+{
+    public class Consumer// : IWeakEventListener
+    {
+        private string name;
+
+        public Consumer(string name)
+        {
+            this.name = name;
+        }
+
+        public void NewCarIsHere(object sender, CarInfoEventArgs e)
+        {
+            Console.WriteLine("                响应事件 sender {0}: args: {1}", name, e.Car);
+        }
+
+        // 不实现这个接口也可以正常使用?!?!?
+       /* bool IWeakEventListener.ReceiveWeakEvent(Type managerType, object sender, EventArgs e)
+        {
+            NewCarIsHere(sender, e as CarInfoEventArgs);
+            return false;
+        }*/
+
+
+    }
+}
---
+++ 0-----C#/事件 弱事件/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Wrox.ProCSharp.Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 内部发送事件
+            var dealer = new CarDealer();
+
+            var michael = new Consumer("1111");
+
+            // 绑定 dealer 内的 NewCarInfo 事件到 michael.NewCarIsHere 函数
+            WeakEventManager<CarDealer, CarInfoEventArgs>.AddHandler(dealer, "NewCarInfo", michael.NewCarIsHere);
+
+            dealer.NewCar("aaaa");
+
+            // 添加一个响应函数
+            var sebastian = new Consumer("2222");
+            WeakEventManager<CarDealer, CarInfoEventArgs>.AddHandler(dealer, "NewCarInfo", sebastian.NewCarIsHere);
+
+            dealer.NewCar("bbb");
+
+            // 取消 michael 响应函数
+            WeakEventManager<CarDealer, CarInfoEventArgs>.RemoveHandler(dealer, "NewCarInfo", michael.NewCarIsHere);
+
+            dealer.NewCar("ccccc");
+        }
+    }
+}
