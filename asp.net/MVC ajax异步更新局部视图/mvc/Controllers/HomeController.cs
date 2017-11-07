3650cfc737a5eacd18ab115e75022d996f0fe51e
xiongfj <xiongfj@eaysx.cn>
2017-11-07 12:02:43 +0800

    d'd

---
+++ asp.net/MVC ajax异步更新局部视图/mvc/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using mvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult About()
+		{
+			ViewBag.Message = "Your application description page.";
+
+			return View();
+		}
+
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Your contact page.";
+
+			return View();
+		}
+
+		/// <summary>
+		/// 必须有对应的 GetData.cshtml 视图
+		/// <div id="data">
+		///		@{Html.RenderAction("GetData");}		// 对应该 Action
+		/// </div>
+		/// </summary>
+		/// <param name="str"></param>
+		/// <returns></returns>
+		public ActionResult GetData(string str)
+		{
+			List<AspUser> list = new List<AspUser>();
+			list.Add(new AspUser("111"+ str));
+			list.Add(new AspUser("222" + str));
+			list.Add(new AspUser("3333" + str));
+			list.Add(new AspUser("44444" + str));
+			return PartialView(list);		// 不能返回 View
+		}
+	}
+}
---
+++ asp.net/MVC ajax异步更新局部视图/mvc/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvc.Models
+{
+	public class AspUser
+	{
+		public AspUser(string id)
+		{
+			Name = "HelloKitty " + id;
+			addr = "address" + id;
+			email = "email " + id;
+		}
+
+		public string Name { get; set; }
+		public string addr { get; set; }
+		public string email { get; set; }
+	}
+}
