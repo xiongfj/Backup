e6a649b860b45323e617dc13d9f7cd917f11b8fe
xiongfj <xiongfj@easyx.cn>
2019-03-06 18:40:41 +0800

    1

---
+++ 0-----C#/序列化 Xml序列化/Email.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+	[Serializable]
+	public class Email
+	{
+		public Email()
+		{
+			EmailTo = "EMailTo";
+			EmailUserName = "Jacksd";
+			EmailPassword = "xxxxxxxxxxxxxx";
+			lists.Add(new People("asd", 132) );
+			lists.Add(new People("44", 12) );
+			lists.Add(new People("s", 34) );
+			lists.Add(new People("bbbg", 62) );
+		}
+
+		// 默认邮件发送目的地
+		public string EmailTo { get; set; }
+
+		// 发送方邮件名
+		public string EmailUserName { get; set; }
+
+		// 发送方密码或者授权码，有些需要登录邮箱设置获取客户端授权码擦才能正确发送
+		public string EmailPassword { get; set; }
+
+		// 服务地址: 像 smtp.163.com
+		public string EmailServer { get; set; }
+
+		// 端口号，smtp 为 25
+		public int EmailServerPort { get; set; }
+
+		// 主题前缀
+		public string EmailSubjectPrefix { get; set; }
+
+		// 是否启用 ssl
+		public bool EmailEnableSsl { get; set; }
+
+		public List<People> lists { set; get; } = new List<People>();
+	}
+}
---
+++ 0-----C#/序列化 Xml序列化/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Reflection;
+using System.Runtime.Caching;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Caching;
+using System.Web.Security;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace ConsoleApp
+{
+	class Program
+	{
+		static void Main()
+		{
+			StringWriter sw = new StringWriter();
+			Email email = new Email();
+
+			XmlSerializer xml = new XmlSerializer(email.GetType());
+
+			XmlSerializerNamespaces ns = new XmlSerializerNamespaces();
+			ns.Add("ns", "ConsoleApp.Email");
+			ns.Add("people", "ConsoleApp.People");
+
+			xml.Serialize(sw, email, ns);
+			Console.WriteLine(sw.ToString());
+
+
+
+			Assembly ass = Assembly.Load("ConsoleApp");
+			Type t = ass.GetType("ConsoleApp.Email");   //参数必须是类的全名
+			if(t == null)
+			{
+				Console.WriteLine("类型不存在");
+				return;
+			}
+			var x = new XmlSerializer(t);
+
+
+			var txt = Encoding.Unicode.GetBytes(sw.ToString());
+			MemoryStream ms = new MemoryStream(txt);
+			var doc = (Email)(x.Deserialize(ms));
+		}
+	}
+
+	[Serializable]
+	public class People
+	{
+		public People() { }
+		public People(string n, int a)
+		{
+			name = n;
+			age = a;
+		}
+		public string name { set; get; }
+		public int age { set; get; }
+	}
+}
